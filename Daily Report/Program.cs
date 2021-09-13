b9e31a65b3e2e261ab7f118083e9ac64e226b545
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-13 15:01:02 -0700

    Add more variables to store answers to questions

--- Daily Report/Program.cs
+++ Daily Report/Program.cs
@@ -14,10 +14,16 @@ namespace Daily_Report
             Console.WriteLine("What Course are you on?");
             string course = Console.ReadLine();
             Console.WriteLine("What page number?");
+            int page = Console.ReadLine();
             Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
+            bool help = Console.ReadLine();
             Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string experience = Console.ReadLine();
             Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
             Console.WriteLine("How many hours did you study today?");
+            int hours = Console.ReadLine();
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!")
             Console.Read();
         }
     }
