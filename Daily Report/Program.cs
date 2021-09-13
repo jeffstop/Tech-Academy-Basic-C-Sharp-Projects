d9a77579c2596b629dce91ba14df3586148c82f3
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-13 14:39:54 -0700

    Add questions to Daily Report

--- Daily Report/Program.cs
+++ Daily Report/Program.cs
@@ -9,6 +9,13 @@ namespace Daily_Report
         {
             Console.WriteLine("The Tech Academy");
             Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What is your name?");
+            Console.WriteLine("What Course are you on?");
+            Console.WriteLine("What page number?");
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            Console.WriteLine("How many hours did you study today?");
             Console.Read();
         }
     }
