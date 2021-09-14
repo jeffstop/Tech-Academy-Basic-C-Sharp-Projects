b260dea41bfe4deae52ea404405662e6da03eee4
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-13 23:45:40 -0700

    Add person 2 questions

--- Math and Comparison Operator/Program.cs
+++ Math and Comparison Operator/Program.cs
@@ -8,11 +8,16 @@ namespace Math_and_Comparison_Operator
         static void Main()
         {
             Console.WriteLine("Anonymous Income Comparison Program");
-            Console.WriteLine("Person1");
+            Console.WriteLine("Person 1");
             Console.WriteLine("Hourly Rate?");
             int person1_hourly_rate = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Hours worked per week?");
             int person1_weekly_hours = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Hourly Rate?");
+            int person2_hourly_rate = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours worked per week?");
+            int person2_weekly_hours = Convert.ToInt32(Console.ReadLine());
             Console.Read();
         }
     }
