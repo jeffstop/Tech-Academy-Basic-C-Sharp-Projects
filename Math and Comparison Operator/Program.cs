5cd2cc26412c663160410f3a2de87294c89865bf
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-14 23:28:38 -0700

    Add annual salaries and compare salaries

--- Math and Comparison Operator/Program.cs
+++ Math and Comparison Operator/Program.cs
@@ -18,6 +18,14 @@ namespace Math_and_Comparison_Operator
             int person2_hourly_rate = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Hours worked per week?");
             int person2_weekly_hours = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Annual salary of Person 1:");
+            int product1 = person1_hourly_rate * person1_weekly_hours * 52;
+            Console.WriteLine(product1);
+            Console.WriteLine("Annual salary of Person 2:");
+            int product2 = person2_hourly_rate * person2_weekly_hours * 52;
+            Console.WriteLine(product2);
+            Console.WriteLine("Does Person 1 make more money than person 2?");
+            Console.WriteLine(product1 >= product2);
             Console.Read();
         }
     }
