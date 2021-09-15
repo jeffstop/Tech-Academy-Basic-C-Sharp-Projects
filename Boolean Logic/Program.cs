82ddfe508383eccc5cc20631112d97bd1be510de
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-15 01:09:17 -0700

    Add comments

--- Boolean Logic/Program.cs
+++ Boolean Logic/Program.cs
@@ -7,13 +7,21 @@ namespace Boolean_Logic
     {
         static void Main()
         {
+            // User age
             Console.WriteLine("What is your age?");
             int age = Convert.ToInt32(Console.ReadLine());
+
+            // DUI
             Console.WriteLine("Have you ever had a DUI? Please enter \"true\" or \"false.\"");
             bool dui = Convert.ToBoolean(Console.ReadLine());
+
+            // Speeding tickets
             Console.WriteLine("How many speeding tickets do you have?");
             int tickets = Convert.ToInt32(Console.ReadLine());
+
+            // Qualified
             Console.WriteLine("Qualified?");
+            // User must be older than 15, have no dui's, and less than 3 speeding tickets to qualify
             bool qualified = (age > 15 && dui == false && tickets < 3);
             Console.WriteLine(qualified);
             Console.Read();
