ddf85457bb655bd5ad4f5c5d5e46f5c2f979776e
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-15 00:47:01 -0700

    Create project
    Add three questions and user input variables

---
+++ Boolean Logic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Boolean_Logic
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            Console.Read();
+        }
+    }
+}
