568865f862860754fb137f496c9cdb3140351074
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-16 14:28:33 -0700

    Create Project
    Add questions

---
+++ Branching/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight:");
+
+            Console.WriteLine("Please enter the package width:");
+
+            Console.WriteLine("Please enter the package height:");
+
+            Console.WriteLine("Please enter the package length:");
+        }
+    }
+}
