38f08c12744d8c9eccb9871ecc14ebda0b76077c
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-16 15:32:19 -0700

    Add package width, height, and length
    Add if statement to terminate program if dimensions too big

--- Branching/Program.cs
+++ Branching/Program.cs
@@ -8,6 +8,7 @@ namespace Branching
         static void Main()
         {
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            // Package Weight
             Console.WriteLine("Please enter the package weight:");
             int weight = Convert.ToInt32(Console.ReadLine());
                 if (weight > 50)
@@ -19,11 +20,28 @@ namespace Branching
                 Environment.Exit(-1);
                 }
 
+            // Package Width
             Console.WriteLine("Please enter the package width:");
+            int width = Convert.ToInt32(Console.ReadLine());
 
+            // Package Height
             Console.WriteLine("Please enter the package height:");
+            int height = Convert.ToInt32(Console.ReadLine());
 
+            // Package Length
             Console.WriteLine("Please enter the package length:");
+            int length = Convert.ToInt32(Console.ReadLine());
+                // 
+                if (width + height + length > 50)
+                {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                // 
+                Console.Read();
+                // 
+                Environment.Exit(-1);
+                }
+
+            
             Console.Read();
         }
     }
