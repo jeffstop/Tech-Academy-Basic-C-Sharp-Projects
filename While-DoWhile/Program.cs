4f0107edeac765c02899519f8b8ed1e7a42fc5b6
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-16 23:08:24 -0700

    Add while and do while statements

---
+++ While-DoWhile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace While_DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // while statement
+            int i = 0;
+            // Loop until i is 5 or greater
+            while (i < 5)
+            {
+                // Writes i for every iteration
+                Console.WriteLine(i);
+                // Add 1 to the value of i
+                i++;
+            }
+
+
+            // do while statement
+            int n = 0;
+            do
+            {
+                // Writes n for every iteration
+                Console.WriteLine(n);
+                // Add 1 to the value of n
+                n++;
+            }
+            // Stops loop once n is 5 or greater
+            while (n < 5);
+
+            Console.Read();
+        }
+    }
+}
