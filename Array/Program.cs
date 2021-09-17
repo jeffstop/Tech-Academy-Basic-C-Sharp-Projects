95f5eda1fb0c54bc3813da66c8c8c8db35e557e1
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-17 00:13:51 -0700

    Add comments

--- Array/Program.cs
+++ Array/Program.cs
@@ -11,27 +11,37 @@ namespace Array
             // String Array
             string[] cars = { "BMW", "Subaru", "Ford", "Audi", "KIA" };
             Console.WriteLine("Select a number between 0-4 to select a car.");
+            // User picks an index in the array
             int stringnumber = Convert.ToInt32(Console.ReadLine());
+                // if statement to close program when a number greater than 4 is chosen
                 if (stringnumber > 4)
                 {
                 Console.WriteLine("You did not select a number between 0-4. Goodbye");
+                // Allow user to read message before closing program
                 Console.Read();
+                // Close program
                 Environment.Exit(-1);
                 }
+            // Print chosen index in the array
             Console.WriteLine(cars[stringnumber]);
 
             // Integer Array
             int[] numbers = { 10, 15, 20, 25, 30 };
             Console.WriteLine("Select a number between 0-4 to select a number.");
+            // User picks an index in the array
             int intnumber = Convert.ToInt32(Console.ReadLine());
-            if (intnumber > 4)
-            {
-                Console.WriteLine("You did not select a number between 0-4. Goodbye");
-                Console.Read();
-                Environment.Exit(-1);
-            }
+                // if statement to close program when a number greater than 4 is chosen
+                if (intnumber > 4)
+                {
+                    Console.WriteLine("You did not select a number between 0-4. Goodbye");
+                    // Allow user to read message before closing program
+                    Console.Read();
+                    // Close program
+                    Environment.Exit(-1);
+                }
+            // Print chosen index in the array
             Console.WriteLine(numbers[intnumber]);
-
+            // Keeps console open
             Console.Read();
         }
     }
