00558aa307105e6eb25966b848331cb055ae8d1b
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-17 00:03:06 -0700

    Create "cars" array

---
+++ Array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = { "BMW", "Subaru", "Ford", "Audi", "KIA" };
+            Console.WriteLine("Select a number between 0-4");
+            int stringnumber = Convert.ToInt32(Console.ReadLine());
+                if (stringnumber > 4)
+                {
+                Console.WriteLine("You did not select a number between 0-4. Goodbye");
+                Console.Read();
+                Environment.Exit(-1);
+                }
+            Console.WriteLine(cars[stringnumber]);
+            Console.Read();
+        }
+    }
+}
