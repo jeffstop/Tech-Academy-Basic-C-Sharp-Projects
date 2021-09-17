662c1472d605ebf4f833b71fb79f68cdf07dcd28
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-17 00:08:09 -0700

    Add "numbers" array

--- Array/Program.cs
+++ Array/Program.cs
@@ -8,8 +8,9 @@ namespace Array
     {
         static void Main(string[] args)
         {
+            // String Array
             string[] cars = { "BMW", "Subaru", "Ford", "Audi", "KIA" };
-            Console.WriteLine("Select a number between 0-4");
+            Console.WriteLine("Select a number between 0-4 to select a car.");
             int stringnumber = Convert.ToInt32(Console.ReadLine());
                 if (stringnumber > 4)
                 {
@@ -18,6 +19,19 @@ namespace Array
                 Environment.Exit(-1);
                 }
             Console.WriteLine(cars[stringnumber]);
+
+            // Integer Array
+            int[] numbers = { 10, 15, 20, 25, 30 };
+            Console.WriteLine("Select a number between 0-4 to select a number.");
+            int intnumber = Convert.ToInt32(Console.ReadLine());
+            if (intnumber > 4)
+            {
+                Console.WriteLine("You did not select a number between 0-4. Goodbye");
+                Console.Read();
+                Environment.Exit(-1);
+            }
+            Console.WriteLine(numbers[intnumber]);
+
             Console.Read();
         }
     }
