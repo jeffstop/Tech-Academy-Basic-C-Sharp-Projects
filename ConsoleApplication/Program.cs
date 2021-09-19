b0a8babf92fc02b327bfa6b056ee398d6605b49d
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-19 13:44:45 -0700

    Create string array and get user input

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArray = { "This is the first string.", "The second string.", "Third string." };
+            Console.WriteLine("Please input a string:");
+            string userString = Console.ReadLine();
+
+        }
+    }
+}
