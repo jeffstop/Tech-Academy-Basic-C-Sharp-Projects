f658b1f710904e68f027c12b60b1c7e68e3ed6af
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-19 19:30:00 -0700

    Add comments

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -10,17 +10,20 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
+            // Create array "stringArray"
             string[] stringArray = { "This is the first string.", "The second string.", "Third string." };
             Console.WriteLine("Please input a string:");
+            // User input
             string userString = Console.ReadLine();
 
+            // For loop to add user input to the end of each element in array "stringArray"
             for (int n = 0; n < stringArray.Length; n++) 
             {
                 string concstring = stringArray[n] + " " + userString;
                 stringArray[n] = concstring; 
             }
             
-            
+            // Foreach loop to print each element in array
             foreach (string i in stringArray) 
             {
                 Console.WriteLine(i);
