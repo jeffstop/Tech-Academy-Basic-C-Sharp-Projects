5b1daa872d0d0f364955b5468a412c46a51594b0
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-23 02:10:12 -0700

    Remove some spacing. Display new list to user and get input

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -15,14 +15,12 @@ namespace ConsoleApplication
             Console.WriteLine("Please input a string:");
             // User input
             string userString = Console.ReadLine();
-
             // For loop to add user input to the end of each element in array "stringArray"
             for (int n = 0; n < stringArray.Length; n++)
             {
                 string concstring = stringArray[n] + " " + userString;
                 stringArray[n] = concstring;
             }
-
             // Foreach loop to print each element in array "stringArray"
             foreach (string i in stringArray)
             {
@@ -39,9 +37,7 @@ namespace ConsoleApplication
             //{
             //Console.WriteLine(infinite);
             //}
-
             Console.WriteLine("Fixed infinite loop:");
-
             // Fixed infinite loop
             bool infinite = true;
             while (infinite == true)
@@ -62,7 +58,6 @@ namespace ConsoleApplication
                 Console.WriteLine(j);
                 j++;
             }
-
             Console.WriteLine("Loop comparison using <=:");
             // Loop comparison using "<="
             int r = 0;
@@ -81,27 +76,21 @@ namespace ConsoleApplication
             strlist.Add("First string");
             strlist.Add("Second string");
             strlist.Add("Third string");
-
             // Display list to make searching it easier for user
             foreach (var str in strlist)
             {
                 Console.WriteLine(str);
             }
-
             Console.WriteLine("Please input your search request for the above list:");
-
             // User search input
             string searchstring = Console.ReadLine();
-
             // Bool variable for search results. False is default and means the search did not match any item on the list, while true is a match for an item
             bool found = false;
-
             // Iterate each item in list
             foreach (var str in strlist)
             {
                 // Create variable to store index value of each item
                 int index = strlist.IndexOf(str);
-
                 // if statement to determine if search result matches item on list. If true changes bool variable "found" to equal true. Displays message on match
                 if (str == searchstring)
                 {
@@ -122,7 +111,13 @@ namespace ConsoleApplication
             duplist.Add("Duplicate");
             duplist.Add("Not duplicate");
             duplist.Add("Duplicate");
-
+            foreach (var dupstr in duplist)
+            {
+                Console.WriteLine(dupstr);
+            }
+            Console.WriteLine("Please search from the list above:");
+            string dupsearch = Console.ReadLine();
+            bool dupfound = false;
 
 
             Console.Read();
