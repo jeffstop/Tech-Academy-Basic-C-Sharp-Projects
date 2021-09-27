277fe13369a8eb5632a6a773117b6a1e3578f9cd
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-27 01:28:08 -0700

    Create list and add foreach statements

--- Strings and Integers/Program.cs
+++ Strings and Integers/Program.cs
@@ -10,7 +10,21 @@ namespace Strings_and_Integers
     {
         static void Main(string[] args)
         {
+            List<int> numbers = new List<int> { 8, 14, 22, 40, 60 };
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
 
+            Console.WriteLine("Please input a number to divide by each number on the list:");
+            int divnum = Convert.ToInt32(Console.ReadLine());
+            foreach (var num in numbers)
+            {
+                int division = num / divnum;
+                Console.WriteLine(division);
+            }
+
+            Console.Read();
         }
     }
 }
