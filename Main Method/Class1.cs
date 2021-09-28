1ffe4143b763c0df2fd5035d544c78cea40e4a18
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-28 11:47:52 -0700

    Create class and first method

---
+++ Main Method/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class Class1
+    {
+        public static int Addition(int userNum, int y)
+        {
+
+            int result = userNum + y;
+
+            return result;
+        }
+    }
+}
