687201498c51c9d50bd1b81b513d3516932fea63
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-28 01:19:47 -0700

    Create three methods

---
+++ Calling Methods/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    class Class1
+    {
+        public static int Sum(int userNum, int y)
+        {
+            int a = userNum;
+            int b = y;
+
+            int result = a + b;
+
+            return result;
+        }
+
+        public static int Multiply(int userNum, int y)
+        {
+            int a = userNum;
+            int b = y;
+
+            int result = a * b;
+
+            return result;
+        }
+
+        public static int Divide(int userNum, int y)
+        {
+            int a = userNum;
+            int b = y;
+
+            int result = a / b;
+
+            return result;
+        }
+    }
+}
