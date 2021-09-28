6911b5ec00110513cfcc02541ab3295a150ea261
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-28 01:23:30 -0700

    Call methods from class

--- Calling Methods/Program.cs
+++ Calling Methods/Program.cs
@@ -13,6 +13,17 @@ namespace Calling_Methods
             Console.WriteLine("Please enter a number to do three math operations on:");
             int userNum = Convert.ToInt32(Console.ReadLine());
 
+            int result1 = Class1.Sum(userNum, 8);
+            Console.WriteLine($"The sum of {userNum} and 8 equals: {result1}");
+            Console.ReadLine();
+
+            int result2 = Class1.Multiply(userNum, 5);
+            Console.WriteLine($"The product of {userNum} and 5 equals: {result1}");
+            Console.ReadLine();
+
+            int result3 = Class1.Divide(userNum, 2);
+            Console.WriteLine($"The division of {userNum} by 2 equals: {result1}");
+            Console.ReadLine();
         }
     }
 }
