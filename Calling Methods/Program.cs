79ed768a954ae1a1486a20555206157b768b68f9
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-28 01:12:22 -0700

    Create program. Add user input variable

---
+++ Calling Methods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to do three math operations on:");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
