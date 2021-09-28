91914b459e616a1463523d48ade3135ca1df88e2
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-28 15:08:45 -0700

    Create and call method to main program

--- Main Method/Class1.cs
+++ Main Method/Class1.cs
@@ -10,8 +10,14 @@ namespace Main_Method
     {
         public static int Addition(int userNum, int y)
         {
-
             int result = userNum + y;
+
+            return result;
+        }
+
+        public static decimal Multiply(decimal userInput, int y)
+        {
+            int result = (int)userInput * y;
 
             return result;
         }
--- Main Method/Program.cs
+++ Main Method/Program.cs
@@ -13,6 +13,10 @@ namespace Main_Method
             Console.WriteLine("Please enter a number to add to 12:");
             int userNum = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(Class1.Addition(userNum, 12));
+
+            Console.WriteLine("Enter a decimal number to be mulitplied by 5:");
+            decimal userInput = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(Class1.Multiply(userInput, 5));
             Console.ReadLine();
         }
     }
