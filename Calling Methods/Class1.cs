ca6923c348a891b9d37a7b4b96a5cf56d8613ac7
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-28 01:30:52 -0700

    Add comments

--- Calling Methods/Class1.cs
+++ Calling Methods/Class1.cs
@@ -8,6 +8,7 @@ namespace Calling_Methods
 {
     class Class1
     {
+        // Method for adding the user's chosen number to another number
         public static int Sum(int userNum, int y)
         {
             int a = userNum;
@@ -18,6 +19,7 @@ namespace Calling_Methods
             return result;
         }
 
+        // Method for multiplying the user's number
         public static int Multiply(int userNum, int y)
         {
             int a = userNum;
@@ -28,6 +30,7 @@ namespace Calling_Methods
             return result;
         }
 
+        // Method for dividing the user's number
         public static int Divide(int userNum, int y)
         {
             int a = userNum;
--- Calling Methods/Program.cs
+++ Calling Methods/Program.cs
@@ -10,17 +10,21 @@ namespace Calling_Methods
     {
         static void Main(string[] args)
         {
+            // Get a number from user
             Console.WriteLine("Please enter a number to do three math operations on:");
             int userNum = Convert.ToInt32(Console.ReadLine());
 
+            // Add the user's number to 8
             int result1 = Class1.Sum(userNum, 8);
             Console.WriteLine($"The sum of {userNum} and 8 equals: {result1}");
             Console.ReadLine();
 
+            // Multiply user's number by 5
             int result2 = Class1.Multiply(userNum, 5);
             Console.WriteLine($"The product of {userNum} and 5 equals: {result2}");
             Console.ReadLine();
 
+            // Divide user's number by 2
             int result3 = Class1.Divide(userNum, 2);
             Console.WriteLine($"The division of {userNum} by 2 equals: {result3}");
             Console.ReadLine();
