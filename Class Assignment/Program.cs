789dadb4860859ba61909ef810504490fc373982
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-29 21:14:09 -0700

    Update Program.cs
    Add spacing in console. Call the overloaded method

--- Class Assignment/Program.cs
+++ Class Assignment/Program.cs
@@ -14,20 +14,25 @@ namespace Class_Assignment
 
 
             Console.WriteLine("Please enter a number:");
-
-
             int num = Convert.ToInt32(Console.ReadLine());
 
             int a;
             math.MyMethod(num, out a);
             Console.WriteLine(num + " / 2 = " + a);
+            Console.WriteLine();
 
 
             int z;
             math.MetOut(out z);
             Console.WriteLine("Method with output parameters: " + z);
+            Console.WriteLine();
 
 
+            int overload1 = math.MetOverload(5, 10);
+            int overload2 = math.MetOverload(7, 12, 15);
+            Console.WriteLine("First overload method:\n5 + 10 = " + overload1);
+            Console.WriteLine();
+            Console.WriteLine("Second overload method:\n7 + 12 + 15 = " + overload2);
 
             Console.ReadLine();
         }
