51ca8c28aaa5225c5c8de75156719a77fe01d8d6
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-29 23:46:38 -0700

    Update Employee.cs
    Add methods to class and add comments

--- Polymorphism/Employee.cs
+++ Polymorphism/Employee.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace Polymorphism
 {
+    // Employee inherits class "Person" and interface "IQuittable"
     class Employee : Person, IQuittable
     {
+        // Method "SayName"
+        public override void SayName()
+        {
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+        }
 
+        // Method "Quit" writes "Goodbye"
+        public void Quit()
+        {
+            Console.WriteLine("Goodbye");
+        }
     }
 }
