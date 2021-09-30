f8cd44c4a523ae2a2a5b186c03a85b230a4b4f54
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-29 21:34:27 -0700

    Update Program.cs
    Add comments. Display new parameters for method "MetOut"

--- Class Assignment/Program.cs
+++ Class Assignment/Program.cs
@@ -10,24 +10,29 @@ namespace Class_Assignment
     {
         static void Main(string[] args)
         {
+            // Instantiate class
             Math math = new Math();
 
-
+            // Get number from user
             Console.WriteLine("Please enter a number:");
             int num = Convert.ToInt32(Console.ReadLine());
 
+            // Create variable for method output
             int a;
             math.MyMethod(num, out a);
             Console.WriteLine(num + " / 2 = " + a);
             Console.WriteLine();
 
 
+            // Create variable for method with output parameters
             int z;
-            math.MetOut(out z);
-            Console.WriteLine("Method with output parameters: " + z);
+            int y;
+            math.MetOut(out z, out y);
+            Console.WriteLine("Method with output parameters: " + z + " & " + y);
             Console.WriteLine();
 
 
+            // Call and display overloaded method
             int overload1 = math.MetOverload(5, 10);
             int overload2 = math.MetOverload(7, 12, 15);
             Console.WriteLine("First overload method:\n5 + 10 = " + overload1);
@@ -36,6 +41,7 @@ namespace Class_Assignment
             Console.WriteLine();
 
 
+            // Call static class
             Console.WriteLine("This is a static class:\n5 * 5 = " + StaticClass.Product(5, 5));
 
             Console.ReadLine();
