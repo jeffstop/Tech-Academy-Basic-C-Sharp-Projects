e72041f89e215ff51d590800f1551d6e7f060050
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-29 22:40:12 -0700

    Update Employee.cs
    Add comments

--- Methods and Objects/Employee.cs
+++ Methods and Objects/Employee.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace Methods_and_Objects
 {
+    // Inherit from class "Person"
     public class Employee : Person
     {
+        // Int data type property called "Id" 
         public int Id { get; set; }
     }
 }
