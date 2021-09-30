0591f835844431172331500984ae3176fb6b5ea3
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-30 13:46:41 -0700

    Update Employee.cs
    Compare Employee ID with overloaded operator

--- Operators/Employee.cs
+++ Operators/Employee.cs
@@ -7,12 +7,48 @@ using System.Threading.Tasks;
 namespace Operators
 {
     // Inherit from class "Person"
-    public class Employee : Person
+    class Employee : Person, IQuittable
     {
+        public int EmployeeID { get; set; }
+
         // Use SayName() method in class "Employee"
         public override void SayName()
         {
             Console.WriteLine(FirstName + " " + LastName);
+        }
+
+        // Method Quit() writes "Goodbye"
+        public void Quit()
+        {
+            Console.WriteLine("Goodbye");
+        }
+
+        public static bool operator ==(Employee one, Employee two)
+        {
+            if (one.EmployeeID == two.EmployeeID)
+            {
+                Console.WriteLine("Employee ID's are equal");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Employee ID's are not equal");
+                return false;
+            }
+        }
+
+        public static bool operator !=(Employee one, Employee two)
+        {
+            if (one.EmployeeID == two.EmployeeID)
+            {
+                Console.WriteLine("Employee ID's are equal");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Employee ID's are not equal");
+                return true;
+            }
         }
     }
 }
