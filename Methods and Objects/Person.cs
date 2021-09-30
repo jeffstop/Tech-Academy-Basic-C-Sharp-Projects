a25bf204ba82c413361640da93cb646b5cfaeba6
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-29 22:40:30 -0700

    Update Person.cs
    Add comments

--- Methods and Objects/Person.cs
+++ Methods and Objects/Person.cs
@@ -8,9 +8,11 @@ namespace Methods_and_Objects
 {
     public class Person
     {
+        // Give class Person string properties of "FirstName" and "LastName"
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
+        // Method for displaying the first and last name of a "Person"
         public void SayName()
         {
             Console.WriteLine($"Name: {FirstName} {LastName}");
