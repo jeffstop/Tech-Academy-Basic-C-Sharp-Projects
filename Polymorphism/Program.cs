24bc2143e11350bc92867f40f79a4d59129176ac
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-29 23:47:27 -0700

    Update Program.cs
    Display employee object with "SayName" and "Quit" methods

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -10,6 +10,14 @@ namespace Polymorphism
     {
         static void Main(string[] args)
         {
+            // Instantiate Employee as "Sample Student"
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            employee.SayName();
+
+            // Use polymorphism to create an object of IQuittable and call the method "Quit"
+            employee.Quit();
+
+            Console.ReadLine();
         }
     }
 }
