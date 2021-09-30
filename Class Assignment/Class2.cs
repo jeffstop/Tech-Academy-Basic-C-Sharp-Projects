f16d38cb12c1fa533d80bee0064b8841800ae15c
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-29 21:22:00 -0700

    Create Class2.cs
    Create a static class

---
+++ Class Assignment/Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Assignment
+{
+    public static class StaticClass
+    {
+        public static int Product(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
