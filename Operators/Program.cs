6cc113d0ce3dbe96a1d8e7d9525358707cfe9353
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-30 14:03:57 -0700

    Update Program.cs
    Call overloaded method

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -10,6 +10,21 @@ namespace Operators
     {
         static void Main(string[] args)
         {
+            // Instantiate Employee as "Sample Student"
+            //Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            //employee.SayName();
+
+            // Use polymorphism to create an object of IQuittable and call the method "Quit"
+            //IQuittable quit = new Employee();
+            //quit.Quit();
+
+            Employee employee1 = new Employee() { FirstName = "Sample", LastName = "Student", EmployeeID = 404 };
+            Employee employee2 = new Employee() { FirstName = "Example", LastName = "Scholar", EmployeeID = 405 };
+
+            bool idcheck = employee1 == employee2;
+            Console.WriteLine(idcheck);
+
+            Console.ReadLine();
         }
     }
 }
