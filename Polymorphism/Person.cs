b2088ae22cfd27017b55a36252ac667da6afc117
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-29 23:38:37 -0700

    Create Person.cs
    Copy/paste from previous assignment

---
+++ Polymorphism/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    // Abstract class
+    public abstract class Person
+    {
+        // String properties for abstract class "Person"
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Abstract method "SayName"
+        public abstract void SayName();
+    }
+}
