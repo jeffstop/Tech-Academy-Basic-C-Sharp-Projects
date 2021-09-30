464ced70f1cde09317aed32ca5436d60ded2e166
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-09-29 23:13:29 -0700

    Update Person.cs

--- Abstract/Person.cs
+++ Abstract/Person.cs
@@ -8,8 +8,8 @@ namespace Abstract
 {
     public abstract class Person
     {
-        public string FirstName;
-        public string LastName;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
         public abstract void SayName();
     }
