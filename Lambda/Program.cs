9e2954067dfa30a2552f30d0350f68b7a872357c
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-10-01 02:36:10 -0700

    Update Program.cs
    Create foreach loop to add all Joe's to a new list. Use lambda functions to get all Joe's and ID's > 5

--- Lambda/Program.cs
+++ Lambda/Program.cs
@@ -23,6 +23,30 @@ namespace Lambda
                 new Employee(){FirstName="Ashley", LastName="Fort", EmployeeID=1},
                 new Employee(){FirstName="Janet", LastName="Hill", EmployeeID=10}
             };
+
+            List<Employee> dupJoe = new List<Employee>();
+            foreach (Employee employee in employeeList)
+            {
+                if (employee.FirstName == "Joe")
+                {
+                    dupJoe.Add(employee);
+                }
+            }
+
+
+            List<Employee> lamJoe = employeeList.Where(x => x.FirstName.Contains("Joe")).ToList();
+
+
+            List<Employee> lamID = employeeList.Where(x => x.EmployeeID > 5).ToList();
+
+
+            Console.WriteLine("Number of employees named \"Joe\":");
+            Console.WriteLine(lamJoe.Count);
+            Console.WriteLine("Number of employees with ID's greater than 5:");
+            Console.WriteLine(lamID.Count);
+
+            Console.ReadLine();
+
         }
     }
 }
