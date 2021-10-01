1dcfb1f4f841f49564a0c95a7bb5e445d8632190
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-10-01 01:50:32 -0700

    Update Program.cs
    Comments

--- Parameters/Program.cs
+++ Parameters/Program.cs
@@ -8,6 +8,7 @@ namespace Parameters
 {
     class Program
     {
+        // Create enum for days of the week
         public enum DaysOfTheWeek
         {
             Sunday,
@@ -22,13 +23,17 @@ namespace Parameters
         {
             try
             {
+                // Get user to enter the day
                 Console.WriteLine("Please enter the day of the week:");
+                // Assign variable to enum data type once entered by user
                 DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), Console.ReadLine(), true);
+                // Display the day
                 Console.WriteLine("Today is {0}", day.ToString());
             }
+            // Catch for if there are any errors
             catch (ArgumentException)
             {
-                Console.WriteLine("Please correctly enter the day of the week.");
+                Console.WriteLine("Please enter an actual day of the week.");
             }
             Console.ReadLine();
         }
