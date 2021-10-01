8967593cf830f8741d5156ee5bf24c5478fc2fbe
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-10-01 01:44:03 -0700

    Update Program.cs
    Create enum and try/catch block

--- Parameters/Program.cs
+++ Parameters/Program.cs
@@ -8,8 +8,27 @@ namespace Parameters
 {
     class Program
     {
+        public enum DaysOfTheWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Please enter the day of the week:");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter correctly enter the day of the week.");
+            }
+            Console.ReadLine();
         }
     }
 }
