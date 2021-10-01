e526a4498081072ec499a2ad3cecb441bf2c9b33
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-10-01 02:13:45 -0700

    Update Program.cs
    Create list of employees

--- Lambda/Program.cs
+++ Lambda/Program.cs
@@ -10,6 +10,19 @@ namespace Lambda
     {
         static void Main(string[] args)
         {
+            List<Employee> employeeList = new List<Employee>()
+            {
+                new Employee(){FirstName="", LastName="", EmployeeID=3},
+                new Employee(){FirstName="Joe", LastName="", EmployeeID=7},
+                new Employee(){FirstName="", LastName="", EmployeeID=2},
+                new Employee(){FirstName="", LastName="", EmployeeID=8},
+                new Employee(){FirstName="", LastName="", EmployeeID=9},
+                new Employee(){FirstName="", LastName="", EmployeeID=6},
+                new Employee(){FirstName="", LastName="", EmployeeID=11},
+                new Employee(){FirstName="Joe", LastName="", EmployeeID=4},
+                new Employee(){FirstName="", LastName="", EmployeeID=1},
+                new Employee(){FirstName="", LastName="", EmployeeID=10}
+            };
         }
     }
 }
