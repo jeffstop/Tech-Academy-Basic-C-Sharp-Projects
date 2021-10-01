fff6f73c03f3a1abaa6735a233ffa13e901291bd
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-10-01 02:06:25 -0700

    Create Employee.cs
    Add SayName() and Quit() methods

---
+++ Lambda/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    // Inherit from class "Person"
+    class Employee : Person
+    {
+        public int EmployeeID { get; set; }
+
+        // Use SayName() method in class "Employee"
+        public override void SayName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+
+        // Method Quit() writes "Goodbye"
+        public void Quit()
+        {
+            Console.WriteLine("Goodbye");
+        }
+    }
