4c0fd91eb05bae8107b9fb5e3b19e5e72afcb564
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-10-03 20:37:47 -0700

    Create Program.cs
    Print current date and time to console

---
+++ DateTimeAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime DT = DateTime.Now;
+            Console.WriteLine(DT);
+
+            Console.Read();
+        }
+    }
+}
