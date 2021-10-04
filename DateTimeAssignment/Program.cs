e0fc1eb0914a5a5d3d2f6e13be66d87f4bcf4db7
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-10-03 20:43:25 -0700

    Update Program.cs
    Comments

--- DateTimeAssignment/Program.cs
+++ DateTimeAssignment/Program.cs
@@ -10,9 +10,11 @@ namespace DateTimeAssignment
     {
         static void Main(string[] args)
         {
+            // Display current date and time
             DateTime DT = DateTime.Now;
             Console.WriteLine(DT);
 
+            // Add user number to current time then display it
             Console.WriteLine("Please enter a number:");
             int userNum = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(userNum + " hours from now is: " + DT.AddHours(userNum));
