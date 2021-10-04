482ae5cf3e0842b609c4a3c32c42fdf355a32e4c
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-10-03 23:18:49 -0700

    Update Program.cs
    Create class "Dog"

--- Constructor/Program.cs
+++ Constructor/Program.cs
@@ -8,8 +8,34 @@ namespace Constructor
 {
     class Program
     {
+        public class Dog
+        {
+            const string Species = "Canis familiaris";
+            public string Breed { get; set; }
+            public int Age { get; set; }
+
+            public Dog (string breed, int age)
+            {
+                Breed = breed;
+                Age = age;
+            }
+
+            public Dog(string breed) : this(breed, 3)
+            {
+
+            }
+
+            public Dog(int age) : this("Australian Shepherd", age)
+            {
+
+            }
+        }
+
         static void Main(string[] args)
         {
+            var dog1 = new Dog("Australian Shepherd");
+            Console.WriteLine(dog1.Breed + " . Age: " + dog1.Age);
+            Console.ReadLine();
         }
     }
 }
