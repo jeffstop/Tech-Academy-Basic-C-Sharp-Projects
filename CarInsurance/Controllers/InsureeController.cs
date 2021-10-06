1bf80fb9a3e4b2a3b6b066606d96a48f1289c069
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-10-05 23:15:15 -0700

    Update InsureeController.cs
    Calculate quote for car insurance

--- CarInsurance/Controllers/InsureeController.cs
+++ CarInsurance/Controllers/InsureeController.cs
@@ -48,6 +48,63 @@ namespace CarInsurance.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
         {
+            decimal quote = 50;
+
+            // Under 18
+            if (DateTime.Now.Year -insuree.DateOfBirth.Year <= 18)
+            {
+                quote += 100;
+            }
+
+            // Between 19 & 25
+            if (DateTime.Now.Year - insuree.DateOfBirth.Year >= 19 && DateTime.Now.Year - insuree.DateOfBirth.Year <= 25)
+            {
+                quote += 50;
+            }
+
+            // Over 25
+            if (DateTime.Now.Year - insuree.DateOfBirth.Year > 25)
+            {
+                quote += 25;
+            }
+
+            // Car year before 2000 and after 2015
+            if (insuree.CarYear < 2000 || insuree.CarYear > 2015)
+            {
+                quote += 25;
+            }
+
+            // Car make "Porsche" and model "911 Carrera"
+            if (insuree.CarMake.ToLower() == "Porsche")
+            {
+                quote += 25;
+                if (insuree.CarModel.ToLower() == "911 Carrera")
+                {
+                    quote += 25;
+                }
+            }
+
+            // Add to quote for every speeding ticket
+            if (insuree.SpeedingTickets > 0)
+            {
+                quote = insuree.SpeedingTickets * 10 + quote;
+            }
+
+            // DUI adds 25% to total
+            if (insuree.DUI == true)
+            {
+                quote = quote * 0.25M + quote;
+            }
+
+            // Full coverage adds 50% to total
+            if (insuree.CoverageType == true)
+            {
+                quote = quote * 0.5M + quote;
+            }
+
+            insuree.Quote = quote;
+
+            
             if (ModelState.IsValid)
             {
                 db.Insurees.Add(insuree);
